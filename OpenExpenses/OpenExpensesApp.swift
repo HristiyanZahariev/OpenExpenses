7d68f96794c154035b2375693b53c9e73acaf583
Hristiyan Zahariev <ihzahariev@gmail.com>
2023-12-12 22:12:29 +0000

    Initial SwiftUI setup

---
+++ OpenExpenses/OpenExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenExpensesApp.swift
+//  OpenExpenses
+//
+//  Created by Hristiyan Zahariev on 12.12.23.
+//
+
+import SwiftUI
+
+@main
+struct OpenExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
